a3bf7bbdd9304d6fafd85414d6f7f2d47d10f1a8
Heiko Seeberger <mail@heikoseeberger.de>
2016-04-13 16:20:14 +0200

    [gabbler-user] Scaffolding, add dummy UserApi (groll:initial)

---
+++ bin/run-gabbler-user.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+n=0
+if [ -n "$1" ]; then
+    n="$1"
+fi
+tag=latest
+if [ -n "$2" ]; then
+    tag="$2"
+fi
+
+docker run \
+  --detach \
+  --name gabbler-user-${n} \
+  --publish 800${n}:8000 \
+  hseeberger/gabbler-user:${tag}
